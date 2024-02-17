858b43bbbcc1d61e984bee3b79abdeba230b23c9
SuperKarine <119334722+SuperKarine@users.noreply.github.com>
2024-02-17 01:17:27 +0100

    erreur l24 membres

--- Garage_VParrot/Espace admin/bannir.php
+++ Garage_VParrot/Espace admin/bannir.php
@@ -1,12 +1,30 @@
-//Déclaré la session et bdd pour bannir un membre
+
 <?php
+//Déclaré la session et bdd pour bannir un membre
 session_start();
-$bdd =new PDO('mysql:host=localhost;dbname=espace_admin;', 'root', 'root');
+$bdd =new PDO('mysql:host=localhost:8889;dbname=espace_admin;', 'root', 'root');
 //je récupère l'identifiant qui a été mis dans l'url
 if(isset($_GET['id']) AND !empty($_GET['id'])){
-    //Affichage message erreur
-}else{
-    echo "L'identifiant n'a pas été récupéré";
-}
 
+    //Affichage message erreur
+    $getid = $_GET['id'];
+    $recupUser = $bdd->prepare('SELECT * FROM connect WHERE id = ?');
+    $recupUser-> execute(array($getid));
+    if($recupUser->rowCount() > 0){
+
+         // création système pour banir l'utilisateur
+
+        $bannirUser = $bdd->prepare('DELETE FROM connect WHERE id = ?');
+        //Puis j'execute cette requête
+        $bannirUser->execute(array($getid));
+
+        // je dirige l'utilisateur vers la page de membres
+        header('Location: membres.php');
+
+        }else {
+        echo "Aucun membres n'a été trouvé";
+        }
+    }else{
+        echo "L'identifiant n'a pas été récupéré";
+    }
 ?>
---
+++ Garage_VParrot/Espace admin/publier_article.php
@@ -0,0 +1,48 @@
+<?php
+//Sécurité
+session_start();
+$bdd = new PDO('mysql:host=localhost:8889;dbname=espace_admin;', 'root', 'root');
+if (!$_SESSION['mdp']){
+    header('Location: connexion.php');
+}
+
+//Il faut créer la table article dans la bdd
+if(isset($_POST['envoi'])){
+    if(!empty($_POST['marque']) AND !empty($_POST['prix'])) {
+        $marque = htmlspecialchars($_POST['marque']);
+//Pour mettre des sauts de lignes
+        $prix = nl2br(htmlspecialchars($_POST['prix']));
+//insérer du contenu dans la nouvelle bdd en créant une nouvelle requête
+
+        $insererArticle = $bdd->prepare('INSERT INTO voitures(marque, prix)VALUES(?, ?)');
+        $insererArticle->excute(array($marque, $prix));
+
+        echo "L'article a bien été envoyé"
+        }else{
+        echo "Veuillez compléter tous les champs...";
+    }
+
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Publier un article</title>
+    <meta charset="UTF-8">
+
+</head>
+<body>
+    <form method="POST" action="">
+        <input type="text" name="marque">
+        <br>
+        <textarea name="prix"></textarea>
+        <br>
+        <input type="submit" name="envoi">
+
+    </form>
+
+</body>
+
+</html>
+
